ca2c45dfd0f7117620bfec74ba74a58fbb4f4c3b
Milos Petkovic <petkovicam@gmail.com>
2025-12-14 15:39:15 +0100

    basic test
    testing the basic functionalities

--- demoapp/lib/main.dart
+++ demoapp/lib/main.dart
@@ -7,10 +7,48 @@ void main() {
 class MyApp extends StatelessWidget {
   MyApp({super.key});
 
-  int age = 27;
+  final double age = 27.5;
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Scaffold(backgroundColor: Colors.deepPurple));
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.deepPurple,
+        appBar: AppBar(
+          title: Text("Padding Widget"),
+          centerTitle: true,
+          backgroundColor: Colors.amber,
+          elevation: 0,
+          leading: Icon(Icons.menu),
+          actions: [IconButton(onPressed: () {}, icon: Icon(Icons.logout))],
+        ),
+        body: Center(
+          child: Container(
+            height: 300,
+            width: 300,
+            // color: Colors.deepOrange,
+            decoration: BoxDecoration(
+              color: Colors.deepOrange,
+              borderRadius: BorderRadius.circular(20),
+            ),
+            // padding: EdgeInsets.all(20),
+            // padding: EdgeInsets.symmetric(horizontal: 20, vertical: 30),
+            padding: EdgeInsets.fromLTRB(10, 20, 30, 40),
+            // padding: EdgeInsets.only(left: 50, top: 10),
+            // padding: EdgeInsets.zero,
+            // padding: EdgeInsets.all(age.clamp(0, 50)),
+            child: Text(
+              "John dos ima ${age.toStringAsFixed(1)} years old",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
